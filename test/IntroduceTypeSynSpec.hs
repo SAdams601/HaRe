3658442ef87d84ac7e065dc76e8c139ae0f730d2
Stephen Adams <sa597@kent.ac.uk>
2015-07-31 15:34:11 +0100

    Updated intro synonym spec to use ct

--- test/IntroduceTypeSynSpec.hs
+++ test/IntroduceTypeSynSpec.hs
@@ -5,6 +5,7 @@ import Test.Hspec
 import Language.Haskell.Refact.Refactoring.IntroduceTypeSyn
 
 import TestUtils
+import System.Directory
 
 main :: IO ()
 main = do
@@ -14,15 +15,17 @@ spec :: Spec
 spec = do
   describe "doIntroduceTypeSyn" $ do
     it "Introduces a small synonym and modifies the type of a single function." $ do
-      res <- introduceTypeSyn logTestSettings testCradle "./test/testdata/IntroduceTypeSyn/TS1.hs" (3,0) "Name" "String"
-      (show res) `shouldBe` "[\"./test/testdata/IntroduceTypeSyn/TS1.hs\"]"
-      diff <- compareFiles "./test/testdata/IntroduceTypeSyn/TS1.refactored.hs"
-                           "./test/testdata/IntroduceTypeSyn/TS1.hs.expected"
+      res <- ct $ introduceTypeSyn logTestSettings testCradle "./IntroduceTypeSyn/TS1.hs" (3,0) "Name" "String"
+      r' <- ct $ mapM makeRelativeToCurrentDirectory res
+      r' `shouldBe` ["./IntroduceTypeSyn/TS1.hs"]
+      diff <- ct $ compareFiles "./IntroduceTypeSyn/TS1.refactored.hs"
+                           "./IntroduceTypeSyn/TS1.hs.expected"
       diff `shouldBe` []
       
     it "Intruduces the synonym for a tuple and changes the type of two functions" $ do
-      res <- introduceTypeSyn defaultTestSettings testCradle "./test/testdata/IntroduceTypeSyn/TS2.hs" (3,0) "Foo" "(String,Int)"
-      (show res) `shouldBe` "[\"./test/testdata/IntroduceTypeSyn/TS2.hs\"]"
-      diff <- compareFiles "./test/testdata/IntroduceTypeSyn/TS2.refactored.hs"
-                           "./test/testdata/IntroduceTypeSyn/TS2.hs.expected"
+      res <- ct $ introduceTypeSyn defaultTestSettings testCradle "./IntroduceTypeSyn/TS2.hs" (3,0) "Foo" "(String,Int)"
+      r' <- ct $ mapM makeRelativeToCurrentDirectory res
+      r' `shouldBe` ["./IntroduceTypeSyn/TS2.hs"]
+      diff <- compareFiles "./IntroduceTypeSyn/TS2.refactored.hs"
+                           "./IntroduceTypeSyn/TS2.hs.expected"
       diff `shouldBe` []
