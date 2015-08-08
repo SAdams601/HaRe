b763aed891c6120b8f72c081a852871f449ee81b
Stephen Adams <sa597@kent.ac.uk>
2015-08-08 20:14:48 +0100

    Moving computers

--- src/Language/Haskell/Refact/Refactoring/IntroduceTypeSyn.hs
+++ src/Language/Haskell/Refact/Refactoring/IntroduceTypeSyn.hs
@@ -83,16 +83,17 @@ addTyDecl (row,col) tyDecl anns (GHC.L l mod) = do
 renameSigs :: Anns -> Module -> (GHC.LHsDecl GHC.RdrName) -> RefactGhc (Anns, Module)
 renameSigs as (GHC.L l m) tydec@(GHC.L _ (GHC.TyClD tyCls)) = do
   let mDecls = GHC.hsmodDecls m
-      (GHC.L _ clsName) = GHC.tcdLName tyCls
-      tyRhs = GHC.tcdRhs tyCls
-      newModDecls = map updateSig mDecls
+      (GHC.L _ clsName) = GHC.tcdLName tyCls     
+      newModDecls = mDecls
   return (as, (GHC.L l (m {GHC.hsmodDecls = newModDecls})))
   where
-    updateSig ldecl
-      | compareSig ldecl tydec = ldecl
-      | otherwise = ldecl
+    tyRhs = GHC.tcdRhs tyCls
+    replaceSig :: GHC.LHsDecl a -> GHC.LHsDecl a
+    replaceSig decl@(GHC.L l (GHC.SigD (GHC.TypeSig _ sig _)))
+      | compareHsType tyRhs sig = decl                                 
+    replaceSig other = other    
 
-compareSig :: (GHC.LHsDecl a) -> (GHC.LHsDecl a) -> Bool
+compareSig :: (GHC.LHsDecl GHC.RdrName) -> (GHC.LHsDecl GHC.RdrName) -> Bool
 compareSig (GHC.L _ (GHC.SigD (GHC.TypeSig _ ty1 _))) (GHC.L _ (GHC.SigD (GHC.TypeSig _ ty2 _))) = True
 compareSig _ _ = False
 
@@ -106,119 +107,10 @@ compareTyList [] [] = True
 compareTyList (ty1:rst1) (ty2:rst2) = (compareHsType ty1 ty2) && (compareTyList rst1 rst2)
 compareTyList _ _ = False
 
-    {-case maybePn of
-    Just _ -> error "Introduce type synonym failed value already defined at source location"
-    Nothing -> do
-      let fullStr = "type " ++ newName ++ " = " ++ typeRep
-          (modName, str) = gfromJust "Tried to get mod name" $ getModuleName parsed
-          buff = stringToStringBuffer fullStr
-      modSum <- GHC.getModSummary modName
-      let newSum = modSum {GHC.ms_hspp_buf = Just buff}
-      typedMod <- GHC.parseModule newSum >>= GHC.typecheckModule
-      let Just (group, _ , _ , _ ) = GHC.tm_renamed_source typedMod
-          [[(GHC.L _ decl)]] = GHC.hs_tyclds group
-          GHC.L _ syn = GHC.td_synRhs $ GHC.tcdTyDefn decl
-          name = mkName newName
-{-TODO syn is the type synonym that needs to be inserted into the renamed AST -}
-      --error $ SYB.showData SYB.TypeChecker 3 syn
-      addTypeDecl fullStr (row, col) decl
-      doIntro name syn 
-      return ()
-mkName :: String -> GHC.Name
-mkName str = GHC.mkSystemName unique occ
-  where unique = getUnique $ fsLit str
-        occ = GHC.mkTyVarOcc str
-addTypeDecl :: String -> SimpPos -> GHC.TyClDecl GHC.Name -> RefactGhc ()
-addTypeDecl declStr loc@(row,col) decl = do
-  --TODO the fast strings in the srclocs should have module information
-  renamed@(group, _ ,_ ,_) <- getRefactRenamed
-  parsed <- getRefactParsed
-  let (modName, str) = gfromJust "Tried to get mod name" $ getModuleName parsed
-      newToks = basicTokenise declStr
-      (startLoc, endLoc) = getStartEndLoc decl
---  error $ SYB.showData SYB.Renamer 3 decl
---  error $ show newToks
-  void $ addToksAfterPos (startLoc,endLoc) (PlaceOffset row col 1) newToks
-    
-  return $ () -- (GHC.hs_tyclds group) ++ [decl]
-  {-r@(group,id,lie,ds) <- getRefactRenamed
-  dflags <- getDynFlags
-  let srcLoc = mkRealSrcLoc (fsLit declStr) row col
-      buff = stringToStringBuffer declStr
-      pres = Lexer.lexTokenStream buff srcLoc dflags
-  case pres of
-    Lexer.POk pstate toks -> do 
-      let l1 = mkSrcLoc (fsLit "") row col
-          l2 = mkSrcLoc (fsLit "") row (col + (length declStr))
-          span = mkSrcSpan l1 l2
-          ldecl = (GHC.L span decl)
-          posTokens = GHC.addSourceToTokens srcLoc buff toks
-          tyclds = GHC.hs_tyclds group
-          newR = (group {GHC.hs_tyclds = tyclds ++ [[ldecl]]}, id, lie, ds)
---      putRefactRenamed newR
---      error $ SYB.showData SYB.Renamer 3 ldecl
---      error $ show posTokens
-      void $ putToksForSpan span posTokens    
-      return ()
-    Lexer.PFailed _ msg -> error $ "Lexer error: " ++  (showSDoc dflags msg)-}
-doIntro :: GHC.Name -> GHC.HsType GHC.Name -> RefactGhc ()
-doIntro name ty = do
-  renamed <- getRefactRenamed
-  let sigs = getTypeSigs renamed
-  everywhereMStaged SYB.Renamer (SYB.mkM replaceTypeVar) sigs
-  return ()
-  where
-    newTyVar = (GHC.HsTyVar name)    
-    replaceTypeVar :: (GHC.LHsType GHC.Name) -> RefactGhc (GHC.LHsType GHC.Name)
-    replaceTypeVar old@(GHC.L l oldTy)
-      | compareHsType oldTy ty
-      = do
-           new <- worker l old
-           return (GHC.L l newTyVar)
-    replaceTypeVar x = return x
-    worker loc old= do
-      let str = GHC.getOccString name
-          fastStr = fsLit str
-          tok = (GHC.L loc (ITconid fastStr), str)
-      (_, expr') <- putDeclToksForSpan loc old [tok]
-      return ()    
---TODO implement this 
-compareHsType :: (GHC.HsType GHC.Name) -> (GHC.HsType GHC.Name) -> Bool
-compareHsType (GHC.HsTyVar n1) (GHC.HsTyVar n2) = n1 == n2
-compareHsType (GHC.HsTupleTy _ lst1) (GHC.HsTupleTy _ lst2) = compareTyList lst1 lst2
-compareHsType _ _ = False
-compareTyList :: [GHC.LHsType GHC.Name] -> [GHC.LHsType GHC.Name] -> Bool
-compareTyList [] [] = True
-compareTyList ((GHC.L _ ty1):rst1) ((GHC.L _ ty2):rst2) = (compareHsType ty1 ty2) && (compareTyList rst1 rst2)
-compareTyList _ _ = False
-getTypeSigs :: (SYB.Data t, SYB.Typeable t) => t -> [GHC.LSig GHC.Name]
+getTypeSigs :: (SYB.Data t, SYB.Typeable t) => t -> [GHC.Sig a]
 getTypeSigs t =
   case res of
     Just a -> a
     Nothing -> error "No type signatures found in module"
-  
-  where res = somethingStaged SYB.Renamer Nothing (Nothing `SYB.mkQ` worker) t
-        worker ((GHC.ValBindsOut _ lst):: GHC.HsValBinds GHC.Name) = Just lst
-adjustPosTokens :: [PosToken] -> Int -> Int -> [PosToken]
-adjustPosTokens [] _ _ = []
-adjustPosTokens (((GHC.L l t), str):rst) row col = (new:rest)
-  where rest = adjustPosTokens rst row col
-        new = ((GHC.L new_span t), str)
-        new_span = updateSrcSpan l row col
-updateSrcLoc :: RealSrcLoc -> Int -> Int -> RealSrcLoc
-updateSrcLoc old_loc row col = mkRealSrcLoc fs new_row new_col
-  where fs = srcLocFile old_loc
-        new_row = row + (srcLocLine old_loc)
-        new_col = col + (srcLocCol old_loc)
-updateSrcSpan :: SrcSpan -> Int -> Int -> SrcSpan
-updateSrcSpan span row col = mkSrcSpan start end
-  where start = wrapSrcLoc $ updateSrcLoc (unwrapSrcLoc $ srcSpanStart span) row col
-        end = wrapSrcLoc $ updateSrcLoc (unwrapSrcLoc $ srcSpanEnd span) row col
-unwrapSrcLoc :: SrcLoc -> RealSrcLoc
-unwrapSrcLoc loc =
-  case loc of
-    RealSrcLoc rl -> rl
-    UnhelpfulLoc _ -> error "Given unhelpful source location."
-wrapSrcLoc :: RealSrcLoc -> SrcLoc
-wrapSrcLoc rl = RealSrcLoc rl
--}
+  where res = SYB.somethingStaged SYB.Parser Nothing (Nothing `SYB.mkQ` worker) t
+        worker sig@(GHC.TypeSig _ _ _) = Just sig
